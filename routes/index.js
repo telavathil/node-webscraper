68bf7c67d7eaae8c33d5524e2513395637dd2504
Tobin Elavathil <tobin.elavathil@gmail.com>
2016-08-02 13:44:09 -0400

    CSV output created

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
+let cheerio = require('cheerio');
+var csvWriter = require('csv-write-stream');
+var fs = require('fs');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  function getSubstackImages() {
+    request("http://substack.net/images/", function(error, response, html) {
+      if (!error && response.statusCode == 200) {
+        let $ = cheerio.load(html);
+        var writer = csvWriter({ headers: ["File Permission", "Absolute URL", "File Type"]});
+        writer.pipe(fs.createWriteStream('out.csv'));
+
+        $('tr').each(function (i, elm) {
+          var filePermission = '';
+          var absUrl = 'http://substack.net/images/';
+          var fileType = ''
+
+          filePermission = $(elm).first().find('code').text().substr(0,12);
+          absUrl += $(elm).first().find('a').text();
+          fileType = $(elm).first().find('a').text().substr(-3);
+
+          writer.write([filePermission,absUrl,fileType]);
+        });
+
+        writer.end();
+      }
+
+
+
+    });
+  }
+  getSubstackImages();
   res.render('index', { title: 'Express' });
 });
 
